0889b3104f90b8c860790982344b4d21f49e81f0
Gustavo <gustavocaris14052002@gmail.com>
2025-07-12 21:51:34 -0300

    Realizando alteracao no inicio do projeto

--- CursoPHP/02aplicacaoWeb.php
+++ CursoPHP/02aplicacaoWeb.php
@@ -4,4 +4,17 @@ $nameFilm = "Homem Aranha";
 $nameFilm = "Gatito de botas";
 $nameFilm = "Xerequ";
 
+
+// 1. Exibindo mensagens com o echo
+echo "Bem-vindo ao mundo PHP!";
+echo "Este é um exemplo com funcionalidades básicas.<br><br>";
+
+// 2. Declaração de variáveis
+// Em PHP, todas as variáveis começam com o símbolo "$" -> tipos de variaveis :
+
+$nome = "Gustavo";          // String (texto)
+$idade = 25;                // Integer (número inteiro)
+$peso = 81.5;               // Float (número com casa decimal)
+$trabalha = true;  
+
 ?>
