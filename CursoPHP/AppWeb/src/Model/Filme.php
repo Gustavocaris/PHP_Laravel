78af7546cebc20698cda663876596eddef669ed8
Gustavo <gustavocaris14052002@gmail.com>
2025-07-20 14:39:19 -0300

    Feat: excluindo a propriedade media e criando uma function para ser calculada a media e me retornr

--- CursoPHP/AppWeb/src/Model/Filme.php
+++ CursoPHP/AppWeb/src/Model/Filme.php
@@ -1,14 +1,20 @@
 <?php
 
+// sempre que eu criar um novo filme, meu array de notas vai ser vazio
 class Filme {
     public string $nome;
     public int $anoLancamento;
     public string $genero;
-    public float $media;
     public array $notas = [];
 
     function avalia(float $nota){
         $notas[] = $nota;
+    }
+    function media(): float {
+        $somaNotas = array_sum($notas);
+        $quantidadeNotas = count($notas);
+
+        return $somaNotas / count($notas);
     }
 }
 
